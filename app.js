6b8c394127e0ba459b3af99db008b43fca2df068
gautam_98 <suddath.gautam98@gmail.com>
2023-06-23 22:29:18 +0530

    fixes

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: '*',
-    methods: ['GET', 'PATCH', 'POST', 'DELETE'],
+    // origin: '*',
+    // methods: ['GET', 'PATCH', 'POST', 'DELETE'],
   }),
 );
 
